a7a61db58162322ad18510edea481ecada066902
Artiqk <dexkyo98@gmail.com>
2022-11-27 00:03:25 +0100

    Added method to get blocks x,y vector from origin

---
+++ shape.py
@@ -0,0 +1,30 @@
+
+class Shape:
+
+    def __init__(self, shape):
+        self.shape = shape
+        self.width, self.height = self.get_sizes()
+
+
+    def get_sizes(self):
+        height = len(self.shape)
+        width  = len(self.shape[0])
+        return width, height
+
+
+    def get_origin(self):
+        x = 0
+        y = len(self.shape) - 1
+        return x, y
+
+
+    def get_all_coordinates(self):
+        origin_x, origin_y = self.get_origin()
+        coordinates = []
+        
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.shape[y][x] == 1:
+                    coordinates.append((abs(origin_x - x), abs(origin_y - y)))
+        
+        return coordinates
