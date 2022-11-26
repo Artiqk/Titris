5a3428bdf1b98505ecbd9f155293906c43c29dba
Artiqk <dexkyo98@gmail.com>
2022-11-27 00:32:06 +0100

    insert_shape now verify if area is available

--- engine.py
+++ engine.py
@@ -1,4 +1,23 @@
 def insert_shape_on_board(shape, board, x, y):
     blocks_coordinates = shape.get_all_coordinates()
+
+    can_be_placed = blocks_positions_valid(blocks_coordinates, x, y, board)
+
+    if can_be_placed:
+        for i in range(len(blocks_coordinates)):
+            block_x = x + blocks_coordinates[i][0]
+            block_y = y - blocks_coordinates[i][1]
+            board.board[block_x][block_y] = 'X'
+    
+
+def block_position_valid(x, y, board):
+    return (board.board[x][y] == '.')
+
+
+def blocks_positions_valid(blocks_coordinates, x, y, board): # FIXME - DRY ? 
     for i in range(len(blocks_coordinates)):
-        board.board[x + blocks_coordinates[i][0]][y - blocks_coordinates[i][1]] = "x"
+        block_x = x + blocks_coordinates[i][0]
+        block_y = y - blocks_coordinates[i][1]
+        if not block_position_valid(block_x, block_y, board):
+            return False
+    return True
