3e6a6940f89740cf97c0d2f3124ce0f1da774710
Artiqk <dexkyo98@gmail.com>
2022-11-27 01:40:47 +0100

    Changed shape character to square

--- board.py
+++ board.py
@@ -20,18 +20,18 @@ class Board:
 
     def display(self):
         for i in range(self.width):
-            print(i, end='  ')
+            print(i, end=' ')
         print()
         for y in range(self.height):
             for x in range(self.width):
-                print(self.board[x][y], end='  ')
+                print(self.board[x][y], end=' ')
             print(y)
         print()
 
     def update(self):
         characters = {
             0: '.',
-            1: 'X'
+            1: chr(int('0x25A0', 0)) # Hex for solid square unicode character
         }
         board = self.board
         for y in range(self.height):
