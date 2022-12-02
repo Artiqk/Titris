84d4b7cef591dd14fab4dbb7326b936e740b978d
Artiqk <dexkyo98@gmail.com>
2022-12-03 00:34:08 +0100

    Removed insert_shape_on_board -> useless

--- engine.py
+++ engine.py
@@ -1,20 +1,8 @@
-def insert_shape_on_board(shape, board, x, y):
-    blocks_coordinates = shape.get_all_coordinates()
-
-    can_be_placed = blocks_positions_valid(blocks_coordinates, x, y, board)
-
-    if can_be_placed:
-        for i in range(len(blocks_coordinates)):
-            block_x = x + blocks_coordinates[i][0]
-            block_y = y - blocks_coordinates[i][1]
-            board.board[block_x][block_y] = 1
-    
-
 def block_position_valid(x, y, board):
     return (board.board[x][y] == '.')
 
 
-def blocks_positions_valid(blocks_coordinates, x, y, board): # FIXME - DRY ? 
+def blocks_positions_valid(blocks_coordinates, x, y, board):
     for i in range(len(blocks_coordinates)):
         block_x = x + blocks_coordinates[i][0]
         block_y = y - blocks_coordinates[i][1]
