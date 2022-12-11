041eea517c37cdaf9892eb456adcf2df04ee3dbb
Artiqk <dexkyo98@gmail.com>
2022-12-11 21:17:12 +0100

    Added buggy functions

--- engine.py
+++ engine.py
@@ -34,10 +34,33 @@ def remove_row(row, shapes):
             if y == row:
                 row_to_remove = (height - shape_row) - 1
                 remove_shape_row(shape, row_to_remove)
-                shape.blocks = shape.update_blocks()
+                shape.update_blocks()
 
 
 def remove_shape_row(shape, row_to_remove):
     width = shape.width
     for i in range(width):
         shape.shape[row_to_remove][i] = 0
+
+
+
+def remove_empty_shapes(shapes):
+    shapes_to_remove_index = []
+    for i in range(len(shapes)):
+        if len(shapes[i].blocks) == 0:
+            shapes_to_remove_index.append(i)
+    shapes_to_remove_index = shapes_to_remove_index[::-1]
+    for index in shapes_to_remove_index:
+        del shapes[index]
+
+
+def fall_shapes(shapes, board):
+    shape_moved = False
+    for shape in shapes:
+        below_y = shape.y - shape.blocks[-1][1] + 1
+        if (below_y < board.height):
+            below_block = board.board[shape.x][below_y]
+            if (below_block == '.'):
+                shape.move(0, 1)
+                shape_moved = True
+    return shape_moved
