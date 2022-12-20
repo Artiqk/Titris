5d9bacdc0655af653edb8e22788d177c526ed82c
Artiqk <dexkyo98@gmail.com>
2022-12-20 16:55:13 +0100

    Fixed fall_shapes function

--- engine.py
+++ engine.py
@@ -35,6 +35,7 @@ def remove_row(row, shapes):
                 row_to_remove = (height - shape_row) - 1
                 remove_shape_row(shape, row_to_remove)
                 shape.update_blocks()
+    
 
 
 def remove_shape_row(shape, row_to_remove):
@@ -49,18 +50,53 @@ def remove_empty_shapes(shapes):
     for i in range(len(shapes)):
         if len(shapes[i].blocks) == 0:
             shapes_to_remove_index.append(i)
+
     shapes_to_remove_index = shapes_to_remove_index[::-1]
+
     for index in shapes_to_remove_index:
         del shapes[index]
 
 
 def fall_shapes(shapes, board):
+    shapes.sort(key=lambda x: x.y, reverse=True)
+    # print([str(shape) for shape in shapes])
     shape_moved = False
     for shape in shapes:
-        below_y = shape.y - shape.blocks[-1][1] + 1
-        if (below_y < board.height):
-            below_block = board.board[shape.x][below_y]
-            if (below_block == '.'):
-                shape.move(0, 1)
-                shape_moved = True
+        if below_shape_free(shape, board):
+            shape.move(0, 1)
+            shape_moved = True
     return shape_moved
+
+
+def below_shape_free(shape, board): # FIXME - Rename function
+    bottom_block_y = shape.blocks[-1][1] # -1 -> dernier block de la liste. 1 -> y.
+    inside_field = (shape.y - bottom_block_y + 1) < board.height
+    if not inside_field:
+        return False
+    return all([below_block_free(block, shape, board) for block in shape.blocks])
+
+
+def below_block_free(block, shape, board):
+    block_x = shape.x + block[0]
+    block_y = shape.y - block[1]
+    below_y = block_y + 1
+
+    if below_y >= board.height:
+        return False
+
+    below_y_block = board.board[block_x][below_y]
+    
+    if type(below_y_block) is dict:
+        return is_block_from_shape(block_x, below_y, shape)
+    
+    return True
+
+
+def is_block_from_shape(block_x, block_y, shape):
+    for block in shape.blocks:
+        x = shape.x + block[0]
+        y = shape.y - block[1]
+        if (x == block_x) and (y == block_y):
+            return True
+    return False
+        
