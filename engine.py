37cba843f197504da79e373deade30ed6e045906
Artiqk <dexkyo98@gmail.com>
2022-12-20 18:45:31 +0100

    Refactored shape_position_valid

--- engine.py
+++ engine.py
@@ -2,11 +2,11 @@ def block_position_valid(x, y, board):
     return (board.board[x][y] == '.')
 
 
-def blocks_positions_valid(blocks_coordinates, x, y, board):
-    for i in range(len(blocks_coordinates)):
-        block_x = x + blocks_coordinates[i][0]
-        block_y = y - blocks_coordinates[i][1]
-        if not block_position_valid(block_x, block_y, board):
+def shape_position_valid(shape, new_x, new_y, board):
+    for block in shape.blocks:
+        x = new_x + block[0]
+        y = new_y - block[1]
+        if not block_position_valid(x, y, board):
             return False
     return True
 
