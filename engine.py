a9fe86890bbf3d68cc3db49dde4575f569b89f02
Artiqk <dexkyo98@gmail.com>
2023-01-19 18:49:42 +0100

    Reworked shape selection

--- engine.py
+++ engine.py
@@ -13,6 +13,8 @@ from os import system
 import numpy as np
 import random
 import string
+from pyfiglet import Figlet
+
 
 
 colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
@@ -81,7 +83,6 @@ def handle_shapes_fall(shapes, board):
     return shape_moved
 
 
-
 def below_shape_free(shape, board): # FIXME - Rename function
     bottom_block_y = shape.blocks[-1][1] # -1 -> dernier block de la liste. 1 -> y.
     inside_field = (shape.y - bottom_block_y + 1) < board.height
@@ -119,9 +120,11 @@ def is_block_from_shape(block_x, block_y, shape): # FIXME - Move to class Shape 
 
 
 
-def draw_shapes_on_line(shapes, add_digits=True):
+def draw_shapes_on_line(shapes, offset=1, add_digits=True):
     solid_square = chr(int('0x25A0', 0))
     height = len(max(shapes, key=len))
+
+    shape_number = 1
 
     for i in range(height):
         for shape in shapes:
@@ -134,13 +137,18 @@ def draw_shapes_on_line(shapes, add_digits=True):
             else:
                 print(' ' * (len(shape[0]) * 2), end='')
             print(' ', end=' ')
+            shape_number += 1
+
         print()
         height -= 1
 
     if (add_digits):
+        digit_offset = 0
         for i in range(len(shapes)):
-            print(str(i + 1) + '.', end='')
-            print(' ' * (len(shapes[i][0]) * 2), end='')
+            print(str(i + offset) + '.', end='')
+            if (i + offset) >= 10:
+                digit_offset = -1
+            print(' ' * ((len(shapes[i][0]) * 2) + digit_offset), end='')
     print()
     print()
                 
@@ -159,13 +167,44 @@ def draw_shape(shape, color): # FIXME - Move to class Shape ?
     print()
 
 
-def check_game_over(fail_counter):
+def get_player_name():
+    print()
+    player = input("Veuillez entrer votre nom : ")
+    return player
+
+
+def get_player_best_score(file):
+    current_best_score = ''
+
+    with open(file, "r") as score_file:
+        current_best_score = score_file.read()
+
+    best_score_data = current_best_score.split(":")
+    best_player, best_score = best_score_data[0], best_score_data[1]
+    return best_player, best_score
+
+
+def update_best_score(player, score, file):
+    with open(file, "w") as score_file:
+        score_file.write(f"{player}:{score}")
+
+
+def check_game_over(fail_counter, score):
     if fail_counter == 3:
-        print("Game Over!")
+        title = Figlet()
+        print(title.renderText("GAME OVER !"))
+
+        player = get_player_name()
+
+        best_player, best_score = get_player_best_score("score.txt")
+
+        if int(score) > int(best_score):
+            update_best_score(player, score, "score.txt")
+
         exit(1)
 
 
-def get_shape_coords(shape, board):
+def get_shape_coords(shape, board, score):
     x = -1
     y = -1
 
@@ -186,28 +225,52 @@ def get_shape_coords(shape, board):
         if not (x_valid and y_valid):
             fail_counter += 1
         
-        check_game_over(fail_counter)
+        check_game_over(fail_counter, score)
 
     return x, y
 
 
-def get_next_shape(map_shape):
-    next_shape = default_shapes[random.choice(list(default_shapes.keys()))]
+def get_next_shape(map_shape, pol_shape, score):
+    custom_shapes = shape_settings[map_shape]
     shape_color = random.choice(colors)
+
+    if pol_shape == "based_on_score" and int(score) < 500:
+        next_shape = default_shapes[random.choice(list(default_shapes.keys()))]
+    else:
+        next_shape = custom_shapes[random.choice(list(custom_shapes.keys()))]
+
     return next_shape, shape_color
 
 
-def choose_next_shape(map_shape, number_of_shapes=3): # Ce parametre permet de definir combien de shape on propose à l'utilisateur => par défaut il est à 3
+def draw_shapes_on_lines(shapes_choice):
+    if len(shapes_choice) > 15:
+        draw_shapes_on_line(shapes_choice[:15])
+        draw_shapes_on_line(shapes_choice[15:], 16)
+    else:
+        draw_shapes_on_line(shapes_choice)
+
+
+def choose_next_shape(map_shape, score, number_of_shapes=3, pol_shape="three_random"): # Ce parametre permet de definir combien de shape on propose à l'utilisateur => par défaut il est à 3
     shapes_choice = []
     shapes_colors = []
 
-    for i in range(number_of_shapes):
-        next_shape, shape_color = get_next_shape(map_shape)
-        shapes_choice.append(next_shape)
-        shapes_colors.append(shape_color)
+    if pol_shape == "three_random" or pol_shape == "based_on_score":
+        number_of_shapes = 3
+    elif pol_shape == "all":
+        number_of_shapes = len(shape_settings[map_shape])
 
-    draw_shapes_on_line(shapes_choice)
+    # On ajoute des shapes dans la liste sans doublon
+    while len(shapes_choice) < number_of_shapes:
+        next_shape, shape_color = get_next_shape(map_shape, pol_shape, score)
+
+        if not next_shape in shapes_choice:
+            shapes_choice.append(next_shape)
+            shapes_colors.append(shape_color)
+
+    draw_shapes_on_lines(shapes_choice)    
+
     chosen_shape = -1
+
     while chosen_shape < 1 or chosen_shape > number_of_shapes:
         chosen_shape = input(f"Choisissez la pièce que vous voulez utiliser (1-{number_of_shapes}) : ")
         if chosen_shape.isdigit(): # Cette condition gère une partie des entrées indésirables
@@ -215,17 +278,18 @@ def choose_next_shape(map_shape, number_of_shapes=3): # Ce parametre permet de d
         else: # Celle-là aussi
             chosen_shape = -1
     index = chosen_shape - 1
+
     return shapes_choice[index], shapes_colors[index]
 
 
-def insert_new_shape(shapes, board, map_shape):
+def insert_new_shape(shapes, board, map_shape, score, pol_shape):
     fail_counter = 0
     position_valid = False
-    shape, color = choose_next_shape(map_shape, 10)
+    shape, color = choose_next_shape(map_shape, score, 3, pol_shape)
     draw_shape(shape, color)
 
     while not position_valid:
-        x, y = get_shape_coords(shape, board)
+        x, y = get_shape_coords(shape, board, score)
         new_shape = Shape(shape, x, y, color)
 
         if shape_position_valid(new_shape, x, y, board):
@@ -234,4 +298,4 @@ def insert_new_shape(shapes, board, map_shape):
         else:
             fail_counter += 1
         
-        check_game_over(fail_counter)
+        check_game_over(fail_counter, score)
